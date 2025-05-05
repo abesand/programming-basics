0ad8c9d62eafb4efaaa12a5435b1c8421c4a7c53
Abraham Sandoval <asandoval17@csu.fullerton.edu>
2025-05-04 18:59:59 -0700

    working on stack

--- stack.cpp
+++ stack.cpp
@@ -2,5 +2,12 @@
 #include <stack>
 
 int main(){
+  std::stack<int> st;
+  for(int i = 0; i < 199; i++){
+    st.push(i);
+  }
+  st.pop();
+  int x = st.top();
+  std::cout << x << std::endl;
   return 0;
 }
