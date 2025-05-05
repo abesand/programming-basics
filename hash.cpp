685eefd62fb09b38d509c1903eb090bac57c8305
Abraham Sandoval <asandoval17@csu.fullerton.edu>
2025-05-05 10:21:08 -0700

    very basic hash table, lots of collisions, will resolve

--- hash.cpp
+++ hash.cpp
@@ -1,6 +1,7 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <iostream>
 using namespace std; 
 
 void setHashFunction(map<int, string>& m, vector<string>& v){
@@ -17,9 +18,28 @@ string retrieveHashValue(int key, const vector<string>& v){
 }
 
 int main(){
-  map<int, string> dora {{100, "bob"}, {200, "sara"}, {305, "tom"}};
+  map<int, string> dora {{777, "bob"}, {200, "sara"}, {305, "tom"}};
+  dora.insert({42, "answer"});
+  dora.insert({17, "lucky"});
+  dora.insert({99, "problems"});
+  dora.insert({512, "bytes"});
+  dora.insert({1000, "grand"});
+  dora.insert({25, "quarter"});
+  dora.insert({7, "lucky"});    
+  dora.insert({107, "prime"});
+  dora.insert({307, "collision"});
   vector<string> hashTable;
+  hashTable.resize(10);
+  setHashFunction(dora, hashTable);
+  int x = 0;
+  while(x != 1){
 
+  
+  cin >> x;
+  string hashOutput = retrieveHashValue(x, hashTable);
+
+  cout << hashOutput << endl;
+  }
 
   
   return 0;
